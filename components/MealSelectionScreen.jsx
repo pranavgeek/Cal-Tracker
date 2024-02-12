49e52e5a385cdddfdb73470f5ef349add53d40cb
Pranav Gotawala <p_gotawala@fanshaweonline.ca>
2024-02-12 12:32:12 -0500

    Added search functionality
    Created MealSelectionScreen where user can search for food/meal

--- components/MealSelectionScreen.jsx
+++ components/MealSelectionScreen.jsx
@@ -1,20 +1,30 @@
-// components/MealSelectionScreen.js
-
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, FlatList, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const MealSelectionScreen = ({ route }) => {
-  const { meal } = route.params;
-  const [searchQuery, setSearchQuery] = useState('');
+  const navigation = useNavigation();
+  const { meal, mealTime, updateTotalCalories } = route.params;
+  const showMeal = true;
+  const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
 
-  const USDA_API_KEY = 'RnflfahJlR3tL4JQISfmMDWaqGRBZoLWZLt8Uada';
-  const API_URL = 'https://api.nal.usda.gov/fdc/v1/foods/search/';
+  const USDA_API_KEY = "RnflfahJlR3tL4JQISfmMDWaqGRBZoLWZLt8Uada";
+  const API_URL = "https://api.nal.usda.gov/fdc/v1/foods/";
 
   const searchFood = async () => {
     try {
       const response = await fetch(
-        `${API_URL}?api_key=${USDA_API_KEY}&query=${encodeURIComponent(searchQuery)}`
+        `${API_URL}search?api_key=${USDA_API_KEY}&query=${encodeURIComponent(
+          searchQuery
+        )}`
       );
       const data = await response.json();
 
@@ -22,19 +32,36 @@ const MealSelectionScreen = ({ route }) => {
         setSearchResults(data.foods);
       }
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
 
+  const handleAddFood = (selectedFood) => {
+    // Fetch detailed information for the selected food
+    if (selectedFood.fdcId) {
+      navigation.navigate("MealDetailsScreen", {
+        fdcId: selectedFood.fdcId,
+        mealTime: meal,
+        updateTotalCalories: updateTotalCalories,
+      });
+    }
+
+    console.log(`Selected food: ${selectedFood.description}`);
+  };
+
   useEffect(() => {
-    if (searchQuery !== '') {
+    if (searchQuery !== "") {
       searchFood();
     }
   }, [searchQuery]);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Search for {meal} items</Text>
+      <Text style={styles.title}>
+        Search for{" "}
+        {meal ? `${meal} items` : mealTime ? `${mealTime} items` : "items"}
+      </Text>
+
       <TextInput
         placeholder="Search for food..."
         placeholderTextColor="#999"
@@ -46,11 +73,14 @@ const MealSelectionScreen = ({ route }) => {
         data={searchResults}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item }) => (
-            <View style={styles.resultItem}>
-              <Text style={styles.resultItemText}>{item.description}</Text>
-              {item.fdcId && <Text>Food ID: {item.fdcId}</Text>}
-            </View>
-          )}
+          <TouchableOpacity
+            onPress={() => handleAddFood(item)}
+            style={styles.resultItem}
+          >
+            <Text style={styles.resultItemText}>{item.description}</Text>
+            {item.fdcId && <Text>Food ID: {item.fdcId}</Text>}
+          </TouchableOpacity>
+        )}
       />
     </View>
   );
@@ -61,19 +91,19 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     paddingTop: 50,
-    backgroundColor: '#000',
+    backgroundColor: "#000",
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
-    color: 'white',
+    color: "white",
   },
   searchInput: {
     height: 50,
-    color: 'white',
+    color: "white",
     fontSize: 20,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderWidth: 1,
     borderRadius: 5,
     marginBottom: 20,
@@ -82,11 +112,11 @@ const styles = StyleSheet.create({
   resultItem: {
     padding: 10,
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    borderBottomColor: "#ccc",
   },
   resultItemText: {
     fontSize: 18,
-    color: 'white', // Set the text color to white
+    color: "white",
   },
 });
 
