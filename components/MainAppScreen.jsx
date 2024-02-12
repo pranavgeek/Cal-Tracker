34669f8dd8c363e6960966e7481a23fb6f126609
Pranav Gotawala <p_gotawala@fanshaweonline.ca>
2024-02-12 15:14:56 -0500

    Created main app screen
    Created MainAppScreen where user can see the daily generated calories

--- components/MainAppScreen.jsx
+++ components/MainAppScreen.jsx
@@ -1,16 +1,14 @@
 // components/MainAppScreen.js
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Modal, StyleSheet, TouchableWithoutFeedback } from 'react-native';
-import MealSelectionScreen from './MealSelectionScreen';
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity, Modal, StyleSheet, TouchableWithoutFeedback } from "react-native";
+import MealSelectionScreen from "./MealSelectionScreen";
+import AddedMealsScreen from "./AddedMealsScreen";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const MainAppScreen = ({ route, navigation }) => {
   const { totalCalories } = route.params;
   const [isModalVisible, setModalVisible] = useState(false);
-   const [caloriesDistribution, setCaloriesDistribution] = useState({
-    breakfast: 0,
-    lunch: 0,
-    dinner: 0,
-  });
+  const [consumedCalories, setConsumedCalories] = useState(0);
 
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
@@ -18,25 +16,50 @@ const MainAppScreen = ({ route, navigation }) => {
 
   const handleMealSelection = (meal) => {
     console.log(`Selected meal: ${meal}`);
-    navigation.navigate('MealSelectionScreen', { meal });
+    navigation.navigate("MealSelectionScreen", { meal, updateTotalCalories });
     toggleModal();
   };
 
-  // const divideCalories = () => {
-  //   const remainingCalories = totalCalories - (caloriesDistribution.breakfast + caloriesDistribution.lunch + caloriesDistribution.dinner);
+  const viewAddedMeals = () => {
+    navigation.navigate('AddedMealsScreen');
+  };
 
-  //   setCaloriesDistribution({
-  //     ...caloriesDistribution,
-  //     breakfast: remainingCalories >= 656 ? 656 : remainingCalories,
-  //   });
-  // };
+  const updateTotalCalories = (meals) => {
+    // Calculate consumed calories from meals and update consumedCalories state
+    let consumed = 0;
+    Object.values(meals).forEach(mealArray => {
+      mealArray.forEach(meal => {
+        consumed += meal.nutrients?.calories || 0;
+      });
+    });
+    setConsumedCalories(consumed);
+  };
+
+  const remainingCalories = totalCalories - consumedCalories;
+
+  //Reset Data
+  const resetUserDetails = async () => {
+    try {
+      await AsyncStorage.removeItem('userDetails');
+      navigation.replace('UserInputScreen')
+    } catch (error) {
+      console.error('Error clearing user details:', error);
+    }
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.Nutrition}>Nutrition</Text>
       <View style={styles.nutritionContainer}>
-        <Text style={styles.caloriesText}>Eat up to {totalCalories} cal</Text>
-        <TouchableOpacity onPress={() => { toggleModal()}} style={styles.addButton}>
+        <Text style={styles.caloriesText}>
+          Eat up to {remainingCalories > 0 ? remainingCalories : 0} cal
+        </Text>
+        <TouchableOpacity
+          onPress={() => {
+            toggleModal();
+          }}
+          style={styles.addButton}
+        >
           <Text style={styles.addButtonText}>+</Text>
         </TouchableOpacity>
       </View>
@@ -52,21 +75,23 @@ const MainAppScreen = ({ route, navigation }) => {
         <TouchableWithoutFeedback onPress={toggleModal}>
           <View style={styles.modalContainer}>
             <View style={styles.modalContent}>
-              <Text style={styles.modalTitle}>Select a Meal You Would Like to Track</Text>
+              <Text style={styles.modalTitle}>
+                Select a Meal You Would Like to Track
+              </Text>
               <TouchableOpacity
-                onPress={() => handleMealSelection('Breakfast')}
+                onPress={() => handleMealSelection("Breakfast")}
                 style={styles.menuItem}
               >
                 <Text style={styles.menuItemText}>Breakfast</Text>
               </TouchableOpacity>
               <TouchableOpacity
-                onPress={() => handleMealSelection('Lunch')}
+                onPress={() => handleMealSelection("Lunch")}
                 style={styles.menuItem}
               >
                 <Text style={styles.menuItemText}>Lunch</Text>
               </TouchableOpacity>
               <TouchableOpacity
-                onPress={() => handleMealSelection('Dinner')}
+                onPress={() => handleMealSelection("Dinner")}
                 style={styles.menuItem}
               >
                 <Text style={styles.menuItemText}>Dinner</Text>
@@ -75,6 +100,13 @@ const MainAppScreen = ({ route, navigation }) => {
           </View>
         </TouchableWithoutFeedback>
       </Modal>
+
+      <TouchableOpacity onPress={viewAddedMeals} style={styles.viewMealsButton}>
+        <Text style={styles.viewMealsButtonText}>View Added Meals</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={resetUserDetails} style={styles.viewMealsButton}>
+        <Text style={styles.viewMealsButtonText}>Reset</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -82,21 +114,21 @@ const MainAppScreen = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
+    justifyContent: "flex-start",
+    alignItems: "center",
     padding: 20,
     paddingTop: 50,
-    backgroundColor: '#000'
+    backgroundColor: "#000",
   },
   nutritionContainer: {
     backgroundColor: "whitesmoke",
     padding: 20,
     borderRadius: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 20,
-    width: '100%',
+    width: "100%",
   },
   caloriesText: {
     fontSize: 18,
@@ -104,38 +136,38 @@ const styles = StyleSheet.create({
   addButton: {
     height: 50,
     width: 50,
-    backgroundColor: '#000000',
+    backgroundColor: "#000000",
     borderRadius: 25,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    borderBottomColor: "#ccc",
   },
   addButtonText: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#fff',
+    fontWeight: "bold",
+    color: "#fff",
   },
   modalContainer: {
     flex: 1,
-    justifyContent: 'flex-end',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: "flex-end",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
   modalContent: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     padding: 20,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
   },
   modalTitle: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
   },
   menuItem: {
     paddingVertical: 10,
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    borderBottomColor: "#ccc",
   },
   menuItemText: {
     fontSize: 18,
@@ -144,9 +176,19 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 20,
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
     color: "white",
-  }
+  },
+  viewMealsButton: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#FF8559",
+    borderRadius: 10,
+  },
+  viewMealsButtonText: {
+    fontSize: 18,
+    color: "#fff",
+  },
 });
 
 export default MainAppScreen;
